2912d627cec9fe8cce3b8ba8c6ff5455379214b1
Adrien Perinet <aperinet@bb136-17.adit.mines.edu>
2016-12-04 20:22:51 -0700

    CSCI403 Final Project
    Contributor: Adrien Perinet
    Details: reformatted table for crime database

--- script.sql
+++ script.sql
@@ -1,33 +1,40 @@
 /* \i csci403/Project9/CSCI403Project9/script.sql */
-DROP TABLE IF EXISTS trips;
-DROP TABLE IF EXISTS stations;
+DROP TABLE IF EXISTS crime;
+DROP TABLE IF EXISTS codes;
 
-CREATE TABLE stations(
-	id integer PRIMARY KEY,
-	terminal text,
-	station text,
-	municipal text,
-	lat float,
-	lng float,
-	status text
-	)
-;
-	
-CREATE TABLE trips(
-	id serial PRIMARY KEY,
-	hubway_id bigint,
-	status text,
-	duration integer,
-	start_date timestamp,
-	start_station integer REFERENCES stations(id),
-	end_date timestamp,
-	end_station integer REFERENCES stations(id),
-	bike_number text,
-	subsc_type text,
-	zip_code text,
-	birth_date integer,
-	gender text	
+CREATE TABLE codes (
+	off_code integer,
+	off_code_ext integer,
+	off_type_id text,
+	off_type_name text,
+	off_cat_id text,
+	off_cat_name text,
+	is_crime boolean,
+	is_traffic boolean,
+	PRIMARY KEY (off_code, off_code_ext)
 );
 
-\COPY stations FROM 'data/hubway_stations.csv' WITH (format csv, header true);
-\COPY trips FROM 'data/hubway_trips.csv' WITH (format csv, header true);
+CREATE TABLE crime (
+	inc_id float,
+	off_id float,
+	off_code integer,
+	off_code_ext integer,
+	off_type_id text,
+	off_cat_id text,
+	first_occ_date timestamp,
+	last_occ_date timestamp,
+	reported_date timestamp,
+	inc_addr text,
+	geo_x float,
+	geo_y float,
+	lon float,
+	lat float,
+	dist_id integer,
+	prec_id integer,
+	nbhd_id text,
+	is_crime boolean,
+	is_traffic boolean
+);
+
+\COPY codes FROM 'data/offense_codes.csv' WITH (format csv, header true);
+\COPY crime FROM 'data/crime.csv' WITH (format csv, header true);
