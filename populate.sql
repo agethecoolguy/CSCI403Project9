6fec900f523d493fbc6f593c409b0fdc070aa66b
Adrien Perinet <aperinet@bb136-17.adit.mines.edu>
2016-12-05 15:50:40 -0700

    Merged script.sql and script2.sql into populate.sql

--- populate.sql
+++ populate.sql
@@ -1,7 +1,6 @@
 /* \i csci403/Project9/CSCI403Project9/script.sql */
 DROP TABLE IF EXISTS crime;
 DROP TABLE IF EXISTS codes;
-
 CREATE TABLE codes (
 	off_code integer,
 	off_code_ext integer,
@@ -13,7 +12,6 @@ CREATE TABLE codes (
 	is_traffic boolean,
 	PRIMARY KEY (off_code, off_code_ext)
 );
-
 CREATE TABLE crime_init (
 	inc_id float,
 	off_id float,
@@ -40,7 +38,6 @@ CREATE TABLE crime_init (
 
 \COPY codes FROM 'data/offense_codes.csv' WITH (format csv, header true);
 \COPY crime_init FROM 'data/crime.csv' WITH (format csv, header true);
-
 CREATE TABLE crime (
 	inc_id float,
 	off_id float,
@@ -60,6 +57,55 @@ CREATE TABLE crime (
 
 INSERT INTO crime (inc_id, off_id, off_code, off_code_ext, first_occ_date, last_occ_date, reported_date, geo_x, geo_y, dist_id, prec_id, nbhd_id)
 	SELECT DISTINCT inc_id, off_id, off_code, off_code_ext, first_occ_date, last_occ_date, reported_date, geo_x, geo_y, dist_id, prec_id, nbhd_id
-	FROM crime_init;
-
+	FROM crime_init
+	WHERE dist_id IS NOT NULL;
 DROP TABLE IF EXISTS crime_init;
+
+/*Get the data from http://data.opencolorado.org/dataset/city-and-county-of-denver-liquor-licenses */
+
+DROP TABLE IF EXISTS liquor_locations;
+CREATE TABLE liquor_locations_complete (
+	bfn text,
+	store_name text,
+	address text,
+	license_type text,
+	license_status text,
+	issue_date timestamp with time zone,
+	end_date timestamp with time zone,
+	address_id double precision,
+	address_line1 text,
+	address_line2 text, 
+	city text, 
+	state text, 
+	zip text,
+	council_dist integer,
+	police_dist integer,
+	census_tract integer, 
+	x_coord double precision,
+	y_coord double precision,
+	global_id text
+);
+
+\COPY liquor_locations_complete FROM 'data/liquor_licenses.csv' WITH (format csv, header true);
+CREATE TABLE liquor_locations (
+	bfn text,
+	store_name text,
+	address text,
+	license_type text,
+	issue_date timestamp with time zone,
+	end_date timestamp with time zone,
+	council_dist integer,
+	police_dist integer,
+	census_tract integer, 
+	x_coord double precision,
+	y_coord double precision
+);
+
+INSERT INTO liquor_locations (bfn, store_name, address, license_type,
+		issue_date, end_date, council_dist, police_dist, census_tract, x_coord, y_coord)
+	SELECT DISTINCT bfn, store_name, address, license_type,
+		issue_date, end_date, council_dist, police_dist, census_tract, x_coord, y_coord
+	FROM liquor_locations_complete;
+DROP TABLE IF EXISTS liquor_locations_complete;
+
+
