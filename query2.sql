d6a3c58052f3f4e327f2a2bf278bd81b63d26052
Austin Schussler <aust.schussler@gmail.com>
2016-12-08 10:41:58 -0700

    Query works with normalized table

--- query2.sql
+++ query2.sql
@@ -1,4 +1,4 @@
-/* This query creates two tables comparing the number of crime vs traffic related incidents at night and during the day. */
+/* This query creates a table comparing the number of crime vs traffic related incidents at night vs during the day. */
 
 DROP TABLE IF EXISTS day_night_incident_comparison;
 
@@ -9,17 +9,22 @@ CREATE TABLE day_night_incident_comparison(
 	PRIMARY KEY(time_of_day, violation_type)
 );
 
-/* this section will have to be modified to reflect the normalized tables */
+INSERT INTO day_night_incident_comparison(time_of_day, violation_type, num_incidents)
+	SELECT 'Day', 'Traffic', COUNT(*) FROM crime 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE is_traffic IS TRUE) 
+		AND extract(hour from first_occ_date) >= 7 AND extract(hour from first_occ_date) <= 19;
 
 INSERT INTO day_night_incident_comparison(time_of_day, violation_type, num_incidents)
-	SELECT 'Day', 'Traffic', COUNT(*) FROM crime
-	WHERE is_traffic IS TRUE AND extract(hour from first_occ_date) >= 7 AND extract(hour from first_occ_date) <= 19;
+	SELECT 'Day','Crime', COUNT(*) FROM crime 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE is_crime IS TRUE) 
+		AND extract(hour from first_occ_date) >= 7 AND extract(hour from first_occ_date) <= 19;
+
 INSERT INTO day_night_incident_comparison(time_of_day, violation_type, num_incidents)
-	SELECT 'Day','Crime', COUNT(*) FROM crime
-	WHERE is_crime IS TRUE AND extract(hour from first_occ_date) >= 7 AND extract(hour from first_occ_date) <= 19;
+	SELECT 'Night', 'Traffic', COUNT(*) FROM crime 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE is_traffic IS TRUE) 
+		AND extract(hour from first_occ_date) <= 7 OR extract(hour from first_occ_date) >= 19;
+
 INSERT INTO day_night_incident_comparison(time_of_day, violation_type, num_incidents)
-	SELECT 'Night', 'Traffic', COUNT(*) FROM crime
-	WHERE is_traffic IS TRUE AND extract(hour from first_occ_date) <= 7 OR extract(hour from first_occ_date) >= 19;
-INSERT INTO day_night_incident_comparison(time_of_day, violation_type, num_incidents)
-	SELECT 'Night', 'Crime', COUNT(*) FROM crime
-	WHERE is_crime IS TRUE AND extract(hour from first_occ_date) <= 7 OR extract(hour from first_occ_date) >= 19;
+	SELECT 'Night', 'Crime', COUNT(*) FROM crime 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE is_crime IS TRUE) 
+		AND extract(hour from first_occ_date) <= 7 OR extract(hour from first_occ_date) >= 19;
