29f7e07160d6774bad3834413f8e100b68a5755f
Austin Schussler <aust.schussler@gmail.com>
2016-12-09 09:40:24 -0700

    Final changes made.

--- query2.sql
+++ query2.sql
@@ -1,13 +1,13 @@
-/* This query creates a table comparing the number of crime vs traffic related incidents at night vs during the day. */
+/* Table used to compare the magnitude of incidents per hour at different times of the day*/
 
 DROP TABLE IF EXISTS incident_comparison;
-
 CREATE TABLE incident_comparison(
 	time_of_day text,
 	violation_type text,
 	incidents_per_hour integer,
 	PRIMARY KEY(time_of_day, violation_type)
 );
+/* The following queries are used to insert data into the table.*/
 INSERT INTO incident_comparison(time_of_day, violation_type, incidents_per_hour)
 	SELECT 'ALL', 'ALL', COUNT(*)/24 FROM crime;
 
@@ -33,15 +33,17 @@ INSERT INTO incident_comparison(time_of_day, violation_type, incidents_per_hour)
 
 INSERT INTO incident_comparison(time_of_day, violation_type, incidents_per_hour)
 	SELECT 'Drinking', 'Alcohol-Related', COUNT(*)/9 FROM crime 
-	WHERE off_code IN (SELECT off_code FROM codes WHERE off_type_name = 'Manufacture of liquor' OR off_type_name = 'Illegal sale of liquor' OR 
-		off_type_name = 'Illegal possession of liquor' OR off_type_name = 'Liquor law violation' OR off_type_name = 'Liquor law violation - other' 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE off_type_name = 'Manufacture of liquor' 
+		OR off_type_name = 'Illegal sale of liquor' OR off_type_name = 'Illegal possession of liquor' 
+		OR off_type_name = 'Liquor law violation' OR off_type_name = 'Liquor law violation - other' 
 		OR off_type_name = 'Public intoxication') 
 		AND extract(hour from first_occ_date) >= 15;
 
 INSERT INTO incident_comparison(time_of_day, violation_type, incidents_per_hour)
 	SELECT 'Non-Drinking', 'Alcohol-Related', COUNT(*)/4 FROM crime 
-	WHERE off_code IN (SELECT off_code FROM codes WHERE off_type_name = 'Manufacture of liquor' OR off_type_name = 'Illegal sale of liquor' OR 
-		off_type_name = 'Illegal possession of liquor' OR off_type_name = 'Liquor law violation' OR off_type_name = 'Liquor law violation - other' 
+	WHERE off_code IN (SELECT off_code FROM codes WHERE off_type_name = 'Manufacture of liquor' 
+		OR off_type_name = 'Illegal sale of liquor' OR off_type_name = 'Illegal possession of liquor' 
+		OR off_type_name = 'Liquor law violation' OR off_type_name = 'Liquor law violation - other' 
 		OR off_type_name = 'Public intoxication') 
 		AND extract(hour from first_occ_date) >= 11 AND extract(hour from first_occ_date) <= 15;
 
